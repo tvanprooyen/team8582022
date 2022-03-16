254dc2ed9333a5d5b4f6bac9f674f75fc93766f7
tvanprooyen <tvanprooyen@live.com>
2022-03-16 07:03:25 -0400

    Update 3-16-2022

--- 2022-6391-Swerve-master/src/main/java/frc/robot/subsystems/ClimbingSubsys.java
+++ 2022-6391-Swerve-master/src/main/java/frc/robot/subsystems/ClimbingSubsys.java
@@ -17,6 +17,8 @@ public class ClimbingSubsys extends SubsystemBase {
     private final Joystick driver1 = new Joystick(Constants.Driver1Port);
     private SparkMaxLimitSwitch m_leftForwardLimit;
     private SparkMaxLimitSwitch m_rightForwardLimit;
+    private final PIDController pidRight = new PIDController(0.004, 0, 0);
+    private final PIDController pidLeft = new PIDController(0.004, 0, 0);
 
     private final Joystick driver = new Joystick(Driver1Port);
 
@@ -63,7 +65,8 @@ public class ClimbingSubsys extends SubsystemBase {
         double rightEncoder = getRightEncoder();
         boolean leftLimit = getLeftLimitSwitch();
         double leftEncoder = getLeftEncoder();
-
+        double setpointLeft = 0;
+        double setpointRight = 0;
 
         double axis = driver1.getRawAxis(2) + (-1 * driver1.getRawAxis(3));
 
@@ -79,28 +82,26 @@ public class ClimbingSubsys extends SubsystemBase {
             leftMotorSpeed = -0.5;
             rightMotorSpeed = -0.5; 
         } */
-
-
         
         //-490
-        /* boolean rightHighLimit = rightEncoder <= -290;
+        boolean rightHighLimit = rightEncoder <= -290;
         if(rightLimit){
             rightLiftMotor.getEncoder().setPosition(0);
         }
 
         if(driver1.getRawButton(1)) {
-        if(rightLimit && rightMotorSpeed > 0) {
-            rightMotorSpeed = 0;
-        } 
-        if(rightHighLimit && rightMotorSpeed < 0) {
-            rightMotorSpeed = 0;
-        }
+            if(rightLimit && rightMotorSpeed > 0) {
+                rightMotorSpeed = 0;
+            } 
+            if(rightHighLimit && rightMotorSpeed < 0) {
+                rightMotorSpeed = 0;
+            }
         } else {
             rightMotorSpeed = 0;
-        } */
+        }
         rightLiftMotor.set(rightMotorSpeed);
 
-        /* boolean leftHighLimit = leftEncoder <= -290;
+        boolean leftHighLimit = leftEncoder <= -290;
         if(leftLimit){
             leftLiftMotor.getEncoder().setPosition(0);
         }
@@ -112,11 +113,11 @@ public class ClimbingSubsys extends SubsystemBase {
             if(leftHighLimit && leftMotorSpeed < 0) {
                 leftMotorSpeed = 0;
             }
-            } else {
+        } else {
                 leftMotorSpeed = 0;
-            } */
+        }
             //rightEncoder >= -490 ||
-            leftLiftMotor.set(leftMotorSpeed); 
+        leftLiftMotor.set(-pidRight.calculate(getRightEncoder().get(), setpointRight)); //May need to be positive
         dashboard();
     }
 }
